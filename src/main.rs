e8f202e937510947bc19a7a7b5b89d3c413be77e
Zane <zanehargis@gmail.com>
2024-05-20 16:37:53 -0700

    feat: add line-editing and history for repl using rustyline

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,44 @@
-mod reader;
 mod evaluator;
 mod printer;
+mod reader;
 
-use std::io::{prelude::*, stdin, stdout};
-
-use reader::read;
 use evaluator::eval;
 use printer::print;
+use reader::read;
 
+use rustyline::error::ReadlineError;
+use rustyline::DefaultEditor;
 
-fn repl(prompt: &'static str) -> std::io::Result<()> {
-    let mut buffer = String::new();
-    let stdin = stdin();
-    let mut stdout = stdout();
+const DEFAULT_PROMPT: &str = ">>> ";
+const HISTORY_FILE: &str = ".risphistory";
+
+fn repl(prompt: &'static str) -> rustyline::Result<()> {
+    let mut rl = DefaultEditor::new()?;
+
+    if rl.load_history(HISTORY_FILE).is_err() {
+        eprintln!("No previous history. Creating new file");
+    }
 
     loop {
-        print!("{prompt}");
-        stdout.flush()?;
-
-        stdin.read_line(&mut buffer)?;
-        print(eval(read(&buffer), ""));
-
-        buffer.clear();
+        let line = rl.readline(prompt);
+        match line {
+            Ok(line) => {
+                let _ = rl.add_history_entry(line.as_str());
+                println!("{}", print(eval(read(line.as_str()), "")));
+            }
+            Err(ReadlineError::Interrupted | ReadlineError::Eof) => {
+                break;
+            }
+            Err(e) => return Err(e),
+        }
     }
+
+    let _ = rl.save_history(HISTORY_FILE);
+    Ok(())
 }
 
 fn main() {
-    if let Err(e) = repl("user> ") {
-        panic!("{e}")
+    if let Err(e) = repl(DEFAULT_PROMPT) {
+        panic!("Error: {e}")
     }
 }

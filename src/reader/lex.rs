d73e6db4097c47cde131aa46b1b35f4099dc9b98
Zane <zanehargis@gmail.com>
2024-05-20 19:22:27 -0700

    ref(lex): simplify token types

--- src/reader/lex.rs
+++ src/reader/lex.rs
@@ -1,63 +1,97 @@
+use std::{iter::Peekable, str::Chars};
+
 #[allow(dead_code)]
 #[derive(Debug, PartialEq, PartialOrd)]
 pub enum TokenType {
-    // Literals
-    IntLit(i64),
-    RealLit(f64),
-    // TODO: Maybe add this
-    // RatLit,
-    StrLit(String),
-    CharLit(char),
+    /// Literals
+    /// #t | #f
     BoolLit(bool),
 
+    // TODO: Split this into a Number subtype then add radix and rational number support
+    IntLit(i64),
+    RealLit(f64),
+
+    CharLit(char),
+    StrLit(String),
+
+    /// (
     OpenParen,
+    /// )
     CloseParen,
-    // TODO: Add these
-    // OpenBracket,
-    // CloseBracket,
-    // OpenCurly,
-    // CloseCurly,
-    Hash,
+    /// [
+    OpenBracket,
+    /// ]
+    CloseBracket,
+    /// #(
+    OpenVector,
+    /// ,
+    Comma,
+    /// ,@
+    CommaAt,
+    /// .
+    Dot,
+    /// `
+    Backtick,
 
-    Ident,
-    Symbol,
+    // We say "fuck it. anything else is an identifier"
+    Ident(String),
 
-    // Keywords
-    Arrow,
-    And,
-    Begin,
-    Case,
-    Cond,
-    Define,
-    Delay,
-    Do,
-    Else,
-    If,
-    Lambda,
-    Let,
-    LetStar,
-    LetRec,
-    Or,
-    Quasiquote,
-    Quote,
-    Set,
-    Unquote,
-    UnquoteSplicing,
-
+    /// ;.*\n
+    Comment,
     Eof,
+    // TODO: Maybe have these be lexed separately
+    // Syntactic Keywords
+    //Else,
+    //Arrow,
+    //Define,
+    //Unquote,
+    //UnquoteSplicing,
+
+    //// Expression Keywords
+    //Quote,
+    //Lambda,
+    //If,
+    //Set,
+    //Begin,
+    //Cond,
+    //And,
+    //Or,
+    //Case,
+    //Let,
+    //LetStar,
+    //LetRec,
+    //Do,
+    //Delay,
+    //Quasiquote,
 }
 
 #[allow(dead_code)]
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Copy, Clone)]
 pub struct Span {
-    begin: (usize, usize),
-    end: (usize, usize),
+    pub begin: (usize, usize),
+    pub end: (usize, usize),
+}
+
+impl Span {
+    pub fn incr_col(old: Span) -> Span {
+        Span {
+            end: (old.end.0, old.end.1 + 1),
+            ..old
+        }
+    }
+
+    pub fn incr_line(old: Span) -> Span {
+        Span {
+            end: (old.end.0 + 1, 1),
+            ..old
+        }
+    }
 }
 
 #[allow(dead_code)]
 pub struct Token {
-    kind: TokenType,
-    span: Span,
+    pub kind: TokenType,
+    pub span: Span,
 }
 
 #[allow(dead_code)]
@@ -69,11 +103,38 @@ impl Token {
 
 pub struct Lexer<'src> {
     input: &'src str,
+    stream: Peekable<Chars<'src>>,
     pos: (usize, usize),
 }
 
 impl<'src> Lexer<'src> {
     pub fn new(input: &'src str) -> Self {
-        Self {  input, pos: (0, 0) }
+        Self {
+            input,
+            stream: input.chars().peekable(),
+            pos: (1, 1),
+        }
     }
+
+    pub fn next_token(&mut self) -> Token {
+        self.skip_whitespace();
+        let token_span = Span {
+            begin: self.pos,
+            end: self.pos,
+        };
+
+        Token::new(TokenType::Eof, token_span)
+    }
+
+    fn skip_whitespace(&mut self) {
+        while self.stream.next_if(|ch| ch.is_ascii_whitespace()).is_some() {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn whitespace_test() {}
 }
